706c36d9777ecdebc5b072c003a61e16f8c24d3b
Anselme <njaanselme@gmail.com>
2024-01-16 20:45:27 +0100

    Ajoutez des fichiers projet.

---
+++ projetICT217/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace projetICT217
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
